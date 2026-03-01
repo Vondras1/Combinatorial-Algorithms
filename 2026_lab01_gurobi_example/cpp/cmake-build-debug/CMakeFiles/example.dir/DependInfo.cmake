335fabfeabb76320f5bee15ce4719954fa6bae3b
Vojtěch Vondráček <vondrvo5@fel.cvut.cz>
2026-03-01 23:40:41 +0100

    First rough solution (SP_CC_0) - Go through the constraints again!

---
+++ 2026_lab01_gurobi_example/cpp/cmake-build-debug/CMakeFiles/example.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/modosist/Seafile/Kombinatoricka_optimalizace/KO_2020/Labs/01_gurobi/gurobi-examples/cpp/example.cpp" "/home/modosist/Seafile/Kombinatoricka_optimalizace/KO_2020/Labs/01_gurobi/gurobi-examples/cpp/cmake-build-debug/CMakeFiles/example.dir/example.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/modosist/opt/gurobi810/linux64/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
